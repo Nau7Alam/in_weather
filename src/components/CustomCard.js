7de4c2393b0f5799d3edbc9a263ecd64bf7b991d
Naushad <wizardshad@gmail.com>
2023-03-15 05:19:56 +0530

    Minor debugging

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,6 +1,6 @@
 const CustomCard = ({ type, item, title, selectDay, iconName, detail, degreeCode }) => {
     return (
-        <div onClick={() => type == 'small' ? selectDay(item) : null} className={`day-card ${type === 'big' ? "day-card-big" : "day-card-small"}`}>
+        <div onClick={() => type === 'small' ? selectDay(item) : null} className={`day-card ${type === 'big' ? "day-card-big" : "day-card-small"}`}>
             <h5 className="day-card-date">{title}</h5>
             <div className="day-card-icon" style={{ fontSize: type === 'big' ? '3.2rem' : '2rem' }}>
                 <i className={`wi ${iconName}`}></i>
