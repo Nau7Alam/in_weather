ca6b73e55e0125c3fe978ba12cdf3e975e3c6bb1
Naushad <wizardshad@gmail.com>
2023-03-15 05:15:05 +0530

    Minor changes for deployment

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -5,7 +5,7 @@ const CustomCard = ({ type, item, title, selectDay, iconName, detail, degreeCode
             <div className="day-card-icon" style={{ fontSize: type === 'big' ? '3.2rem' : '2rem' }}>
                 <i className={`wi ${iconName}`}></i>
             </div>
-            <h5 className="day-card-temp">{detail} {type == 'small' ? degreeCode : null}</h5>
+            <h5 className="day-card-temp">{detail} {type === 'small' ? degreeCode : null}</h5>
         </div>
     )
 }
